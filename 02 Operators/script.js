b6cfdc8b64cf519dd1a9710e4bd221c724b7a2bb
TommyPhi <tommy.phi04@gmail.com>
2023-09-28 14:44:21 -0400

    Forked and modified 02-Operators JavaScript

--- 02 Operators/script.js
+++ 02 Operators/script.js
@@ -1,51 +1,28 @@
-let x = 5; y = 10;
-console.log('X:  '+x);
-console.log('Y:  '+y);
-x = y;
-console.log('X:  '+x);
-console.log('Y:  '+y);
-x += y;
-console.log('X:  '+x);
-console.log('Y:  '+y);
-x-= y;
-console.log('X:  '+x);
-console.log('Y:  '+y);
-x *= y;
-console.log('X:  '+x);
-console.log('Y:  '+y);
-x /= y;
-console.log('X:  '+x);
-console.log('Y:  '+y);
-x%=y;
-console.log('X:  '+x);
-console.log('Y:  '+y);
+let bmiEquation = (mass, height) => {
+    let bmi = mass / height ** 2;
+    return bmi;
+}
 
-let markM1 = 78;
-let markH1 = 1.69;
+let markMass=95;
+let markHeight=1.88;
 
-let johnM1 = 92;
-let johnH1 = 1.95;
+let johnMass=85;
+let johnHeight=1.76;
 
-let markBMI1 = markM1/(markH1*markH1);
-let johnBMI1  = johnM1/(johnH1*johnH1);
+let markBmi = bmiEquation(markMass, markHeight);
 
-let markHigherBMI = markBMI1>johnBMI1;
+let johnBmi = bmiEquation(johnMass, johnHeight);
 
-console.log('Mark BMI: '+markBMI1);
-console.log('John BMI: '+johnBMI1);
-console.log('Does mark have a higher BMI than john?: '+markHigherBMI);
+let markHigherBmi;
 
-let markM2 = 95;
-let markH2 = 1.88;
+if(markBmi > johnBmi) {
+    markHigherBmi = true;
+} else {
+    markHigherBmi = false;
+}
 
-let johnM2 = 85;
-let johnH2 = 1.76;
-
-let markBMI2 = markM2/(markH2*markH2);
-let johnBMI2  = johnM2/(johnH2*johnH2);
-
-markHigherBMI = markBMI2>johnBMI2;
-
-console.log('Mark BMI: '+markBMI2);
-console.log('John BMI: '+johnBMI2);
-console.log('Does mark have a higher BMI than john?: '+markHigherBMI);
+if(markHigherBmi) {
+    console.log("Mark has a higher BMI than John")
+} else {
+    console.log("Mark does not have a higher BMI than John")
+}
